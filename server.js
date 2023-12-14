47af8f11e157d98e13f21312f1b0d08848fcac79
dinhdatle <111744077+dinhdatle@users.noreply.github.com>
2023-12-14 22:12:30 +0700

    turn off cors

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ import messageRouter from "./routes/messageRoutes.js";
 import { createServer } from "http";
 import { Server } from "socket.io";
 // import path from "path";
-import cors from "cors";
+// import cors from "cors";
 const app = express();
 
 dotenv.config();
 connectDB();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 const port = process.env.PORT || 5000;
 const client = process.env.CLIENT;
 
@@ -28,11 +28,13 @@ app.use("/api/chat", chatRouter);
 app.use("/api/message", messageRouter);
 
 const httpServer = createServer(app);
-const io = new Server(httpServer, {
-  cors: {
-    origin: client,
-  },
-});
+// const io = new Server(httpServer, {
+//   cors: {
+//     origin: client,
+//   },
+// });
+
+const io = new Server(httpServer);
 
 io.on("connection", (socket) => {
   console.log("Connected to socket.io");
